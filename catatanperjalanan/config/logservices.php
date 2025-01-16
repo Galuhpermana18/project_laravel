b98c8494020640d27dbb25877beb89eaa65734da
Galuhpermana18 <galuhpermana2004@gmail.com>
2025-01-17 01:52:46 +0700

    Delete catatanperjalanan folder

--- catatanperjalanan/config/logservices.php
+++
@@ -1,5 +0,0 @@
-<?php 
-return [
-   'flush' => FALSE,  // change this line for activate / disactivated flush (default = true)
-   'limit' => 10000, // change this line for limit you want to flush (default = 100)
-];
